c7925196c9d409206647d12783110b296e0f7267
BRANYA43 <tbd2504@gmai.com>
2024-12-06 14:51:08 +0200

    Add DRF Standardized Errors

--- src/core/settings/base.py
+++ src/core/settings/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'drf_standardized_errors',
 ]
 
 
--- src/core/settings/rest_framework.py
+++ src/core/settings/rest_framework.py
@@ -1,5 +1,6 @@
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25,
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_standardized_errors.openapi.AutoSchema',
+    'EXCEPTION_HANDLER': 'drf_standardized_errors.handler.exception_handler',
 }
