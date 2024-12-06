25de7adde36bade863f786746c77b1f2a7e7913d
BRANYA43 <tbd2504@gmai.com>
2024-12-06 14:43:07 +0200

    Add DRF-spectacular

---
+++ src/core/settings/drf-spectacular.py
@@ -0,0 +1,5 @@
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Web Comments',
+    'DESCRIPTION': '',
+    'VERSION': '0.1.0',
+}
--- src/core/settings/rest_framework.py
+++ src/core/settings/rest_framework.py
@@ -1,4 +1,5 @@
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
