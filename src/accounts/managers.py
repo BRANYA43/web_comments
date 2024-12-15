b5fb5b3483e66d791074ac854716b420810109b5
BRANYA43 <tbd2504@gmai.com>
2024-12-15 14:07:04 +0200

    Fix setting is_active field for user

--- src/accounts/managers.py
+++ src/accounts/managers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
     def create_user(self, email: str, username: str, password: str, **extra_fields):
-        extra_fields['is_active'] = False
+        extra_fields.setdefault('is_active', False)
         return self._create_user(email, username, password, **extra_fields)
 
     def create_superuser(self, email: str, username: str, password: str, **extra_fields):
