38adbbba9e6eda0d09db0f2a4251ad6cf49cba70
BRANYA43 <tbd2504@gmai.com>
2024-12-16 19:54:42 +0200

    Create CommentListSerializer

--- src/comments/serializers.py
+++ src/comments/serializers.py
@@ -1,1 +1,10 @@
 from rest_framework import serializers  # NOQA
+
+from comments.models import Comment
+
+
+class CommentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('uuid', 'user', 'text', 'updated', 'created')
+        read_only_fields = fields
