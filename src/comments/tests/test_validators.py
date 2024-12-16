13a4fa1657eef0bf82139cd0db2900ea2de29005
BRANYA43 <tbd2504@gmai.com>
2024-12-16 17:20:35 +0200

    Create FileSizeValidator

---
+++ src/comments/tests/test_validators.py
@@ -0,0 +1,42 @@
+from unittest.mock import MagicMock
+
+import pytest
+from django.core.exceptions import ValidationError
+
+from comments.validators import FileSizeValidator
+
+
+class TestFileSizeValidator:
+    validator_class = FileSizeValidator
+
+    @pytest.mark.parametrize('file', [MagicMock(size=i) for i in range(1, 6)])
+    def test_validator_doesnt_raise_error_to_valid_size_in_bytes(self, file):
+        validator = self.validator_class(5, 'b')
+        validator(file)  # not raise
+
+    @pytest.mark.parametrize('file', [MagicMock(size=i * 1024) for i in range(1, 6)])
+    def test_validator_doesnt_raise_error_to_valid_size_in_kb(self, file):
+        validator = self.validator_class(5, 'kb')
+        validator(file)  # not raise
+
+    @pytest.mark.parametrize('file', [MagicMock(size=i) for i in range(6, 11)])
+    def test_validator_raises_error_for_too_large_size_in_bytes(self, file):
+        validator = self.validator_class(5, 'b')
+        with pytest.raises(ValidationError, match='File size must be less or equal 5b.'):
+            validator(file)
+
+    @pytest.mark.parametrize('file', [MagicMock(size=i * 1024) for i in range(6, 11)])
+    def test_validator_raises_error_for_too_large_size_in_kb(self, file):
+        validator = self.validator_class(5, 'kb')
+        with pytest.raises(ValidationError, match='File size must be less or equal 5kb.'):
+            validator(file)
+
+    @pytest.mark.parametrize('invalid_unit', ['', 1, 1.1, None, 'None', 'bbb'])
+    def test_validator_raises_error_for_invalid_unit(self, invalid_unit):
+        with pytest.raises(ValueError, match='Unit must be one of next values:'):
+            self.validator_class(1, invalid_unit)
+
+    @pytest.mark.parametrize('invalid_size', ['', None, object, 'a'])
+    def test_validator_raises_error_for_invalid_max_size(self, invalid_size):
+        with pytest.raises(ValueError, match='Unit must be one of next values:'):
+            self.validator_class(invalid_size, 'b')
---
+++ src/comments/validators.py
@@ -0,0 +1,29 @@
+from typing import Literal
+
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class FileSizeValidator:
+    _coefficients = {
+        'b': 1,
+        'kb': 1024,
+    }
+
+    default_message_error = {'file_too_large': 'File size must be less or equal {max_size}{unit}.'}
+
+    def __init__(self, max_size: int, unit: Literal['b', 'kb']):
+        self._max_size = int(max_size)
+        self._unit = str(unit)
+        try:
+            self._max_size_in_bytes = self._max_size * self._coefficients[str(unit)]
+        except KeyError:
+            raise ValueError(f'Unit must be one of next values: {self._coefficients.keys()}')
+
+    def __call__(self, value, *args, **kwargs):
+        if value.size > self._max_size_in_bytes:
+            raise ValidationError(
+                self.default_message_error['file_too_large'].format(max_size=self._max_size, unit=self._unit),
+                'file_too_large',
+            )
