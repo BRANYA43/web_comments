00db2b63e034bdf01c81440eb30975c5f41ce9be
BRANYA43 <tbd2504@gmai.com>
2024-12-17 16:50:16 +0200

    Register Comment model to admin

--- src/comments/admin.py
+++ src/comments/admin.py
@@ -1,1 +1,18 @@
 from django.contrib import admin  # NOQA
+
+from comments.models import Comment
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'user', 'is_answer', 'created', 'updated')
+    fieldsets = (
+        ('General Info', {'fields': ('uuid', 'user', 'target')}),
+        ('Content', {'fields': ('text', 'image', 'file')}),
+        ('Dates', {'fields': ('created', 'updated')}),
+    )
+    readonly_fields = ('uuid', 'created', 'updated', 'is_answer')
+
+    @admin.display(boolean=True)
+    def is_answer(self, instance) -> bool:
+        return bool(instance.target)
