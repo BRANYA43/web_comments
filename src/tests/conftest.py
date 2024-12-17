65c877f5294702bcc29367477b2cdced67548a1a
BRANYA43 <tbd2504@gmai.com>
2024-12-17 12:34:51 +0200

    Create User and Comment Factory

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,8 +1,40 @@
+import factory.django
 import pytest
+from django.contrib.auth import get_user_model
 from django.test import override_settings
+from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
+from comments.models import Comment
 
+GeneralUser = get_user_model()
+
+
+########################################################################################################################
+# Factories
+########################################################################################################################
+@register
+class UserFactory(factory.django.DjangoModelFactory):
+    email = factory.Faker('email')
+    username = factory.Faker('user_name')
+    password = factory.django.Password('password1234')
+
+    class Meta:
+        model = GeneralUser
+
+
+@register
+class CommentFactory(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    text = factory.Faker('text')
+
+    class Meta:
+        model = Comment
+
+
+########################################################################################################################
+# Fixtures
+########################################################################################################################
 @pytest.fixture()
 def api_client() -> APIClient:
     return APIClient()
