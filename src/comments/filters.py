2fba097e866b9cf15fd2a05c2bcfe4d02b668e24
BRANYA43 <tbd2504@gmai.com>
2024-12-17 16:58:05 +0200

    Add ordering filter to CommentFilter

--- src/comments/filters.py
+++ src/comments/filters.py
@@ -7,6 +7,21 @@ class CommentFilter(django_filters.FilterSet):
     target_is_null = django_filters.BooleanFilter(field_name='target', lookup_expr='isnull')
     target = django_filters.UUIDFilter(field_name='target__uuid', lookup_expr='exact')
 
+    ordering = django_filters.OrderingFilter(
+        fields=(
+            ('user__email', 'email'),
+            ('user__username', 'username'),
+            ('updated', 'updated'),
+            ('created', 'created'),
+        ),
+        field_labels={
+            'user__emai': 'Email',
+            'user__username': 'Username',
+            'updated': 'Date of update',
+            'created': 'Date of creation',
+        },
+    )
+
     class Meta:
         model = Comment
         fields = ['target', 'target_is_null']
