2534b8d44ae887eb82e2cdabb1f4146adc5ee2e1
BRANYA43 <tbd2504@gmai.com>
2024-12-23 17:33:01 +0200

    Fix AppRegistryNotReady

--- src/core/asgi.py
+++ src/core/asgi.py
@@ -14,11 +14,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
 from django.core.asgi import get_asgi_application
 
-from comments import consumers
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 django_asgi_app = get_asgi_application()
+
+from comments import consumers  # NOQA E402
 
 application = ProtocolTypeRouter(
     {
