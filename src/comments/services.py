6654c76d0f2d73582c4d57c8273461e0a9079d6f
BRANYA43 <tbd2504@gmai.com>
2024-12-23 13:40:56 +0200

    Create resize_image service

--- src/comments/services.py
+++ src/comments/services.py
@@ -1,8 +1,37 @@
+from io import BytesIO
 from pathlib import Path
 
+from PIL import Image
 from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.utils.deconstruct import deconstructible
 from django.utils.timezone import now
+
+
+def resize_image(uploaded_image: InMemoryUploadedFile, width=320, height=240) -> InMemoryUploadedFile:
+    uploaded_image.seek(0)
+    image = Image.open(uploaded_image.file)
+    if image.width > width or image.height > height:
+        ratio = min(float(width) / image.size[0], float(height) / image.size[1])
+        w = int(image.size[0] * ratio)
+        h = int(image.size[1] * ratio)
+        resized_image = image.resize((w, h), Image.Resampling.LANCZOS)
+
+        buffer = BytesIO()
+        resized_image.save(buffer, format=image.format)
+        buffer.seek(0)
+
+        resized_uploaded_image = InMemoryUploadedFile(
+            file=buffer,
+            field_name=uploaded_image.field_name,
+            name=uploaded_image.name,
+            content_type=uploaded_image.content_type,
+            size=buffer.getbuffer().nbytes,
+            charset=uploaded_image.charset,
+        )
+        return resized_uploaded_image
+    uploaded_image.seek(0)
+    return uploaded_image
 
 
 @deconstructible
